d9bb03c58a6f0044a61e871b913fed510f6dae22
Prakhar-010 <itsprakhar010@gmail.com>
2026-06-20 02:48:12 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+import os
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+
 from networksecurity.components.data_ingestion import DataIngestion
 import sys
 from networksecurity.exception.exception import CustomException
-from networksecurity.exception.exception import CustomException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
-if __name__=="__main__":
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.artifact_entity import DataIngestionArtifact
+
+if __name__ == "__main__":
     try:
-        trainingpipelineconfig=TrainingPipelineConfig()
-        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion=DataIngestion(dataingestionconfig)
-        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        trainingpipelineconfig = TrainingPipelineConfig()  # ✅ this line was missing
+
+        # Data Ingestion
+        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion = DataIngestion(dataingestionconfig)
+        dataingestionartifact = data_ingestion.initiate_data_ingestion()
         print(dataingestionartifact)
+
+        # Data Validation
+        datavalidationconfig = DataValidationConfig(trainingpipelineconfig)
+        data_vaidation = DataValidation(dataingestionartifact, datavalidationconfig)
+        data_vaidation_artifact = data_vaidation.initiate_data_validation()
+        print(data_vaidation_artifact)
+
     except Exception as e:
-        raise CustomException (e,sys)
+        raise CustomException(e, sys)
